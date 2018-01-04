60ef35bdb830977750467b147db9d2206ceb792e
kg.zheng <kg.zheng@dji.com>
2018-01-04 18:55:35 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,21 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    initWindowEvents () {
+      if (!window) return
+      window.onblur = function (e) {
+        document.title = '您的浏览器崩溃啦/(ㄒoㄒ)/~~'
+      }
+      window.onfocus = function (e) {
+        document.title = '欢迎回来~'
+      }
+    }
+  },
+  created () {
+    this.initWindowEvents()
+  }
 }
 </script>
 
